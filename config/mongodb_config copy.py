0286b502fc0f65cb245b5218c23f46927b7443be
Adam Hill <Cadarn@users.noreply.github.com>
2024-06-18 10:08:13 +0100

    Tidy up files

--- config/mongodb_config copy.py
+++
@@ -1,4 +0,0 @@
-MONGO_URI = "mongodb+srv://adamhill:Prefect-falcon-pallas@prefect-tutorial.haryygz.mongodb.net/?retryWrites=true&w=majority&appName=Prefect-tutorial"
-DB_NAME = "Prefect-tutorial"
-COLLECTION_NAME = "airline-tweet-sentiment"
-
